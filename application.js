31390cce7208295fa44d6c4631ab60e1c512b767
sliman alsliman <silvertears1977@yahoo.com>
2017-07-03 14:25:51 +0200

    ying arround

--- application.js
+++ application.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-    $("button").click(function(){
-        $("p").toggle();
+    $(".btn1").click(function(){
+        $("ul.recipie1").toggle(500);
     });
-});
+
+    $(".btn2").click(function(){
+        $("ul.recipie2").toggle(500);
+    });
+    $(".remove").click(function(){
+        $("li").toggleClass(500);
+    });
+      });
